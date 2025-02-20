f1ddc3cb7b1bdb6553b809fba11d099ef4216963
Natalia Abakumova <n.abakumova@3l.ru>
2025-02-20 16:59:08 +0300

    Formatting permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -33,7 +33,7 @@ class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
-                request.user.is_admin or request.user.is_superuser
+            request.user.is_admin or request.user.is_superuser
         )
 
 
@@ -53,16 +53,16 @@ class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-                or request.user.is_moderator
-                or request.user.is_admin
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_moderator
+            or request.user.is_admin
         )
 
 
@@ -74,6 +74,6 @@ class IsAuthenticatedOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
